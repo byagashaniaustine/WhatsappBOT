15305a83d43511ec174fc6e1c591dff40009ddba
byagashaniaustine <byagashaniaustine@bmail.com>
2025-11-24 18:04:38 +0300

    new template algo

--- services/meta.py
+++ services/meta.py
@@ -15,8 +15,12 @@ logger.setLevel(logging.INFO)
 # -----------------------------------
 ACCESS_TOKEN = os.environ.get("META_ACCESS_TOKEN")
 PHONE_NUMBER_ID = os.environ.get("WA_PHONE_NUMBER_ID")
-API_URL = f"https://graph.facebook.com/v24.0/{PHONE_NUMBER_ID}/messages"
-MEDIA_API_BASE_URL = "https://graph.facebook.com/v24.0/"
+
+# WhatsApp Cloud API version
+API_VERSION = "v22.0"
+
+API_URL = f"https://graph.facebook.com/{API_VERSION}/{PHONE_NUMBER_ID}/messages"
+MEDIA_API_BASE_URL = f"https://graph.facebook.com/{API_VERSION}/"
 
 # ==============================================================
 # SEND SIMPLE WHATSAPP TEXT MESSAGE
